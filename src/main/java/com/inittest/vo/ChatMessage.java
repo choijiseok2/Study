42b0e21aa0365769122b726a7e7f31fa2f999529
gkffh <gkffh@DESKTOP-HUJO144>
2020-08-12 00:42:29 +0900

    2

--- src/main/java/com/inittest/vo/ChatMessage.java
+++ src/main/java/com/inittest/vo/ChatMessage.java
@@ -2,33 +2,11 @@ package com.inittest.vo;
 
 import com.inittest.enums.MessageType;
 
-public class ChatMessage {
+import lombok.Data;
 
+@Data	
+public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
-
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
 }
